3e090aa9a5b3f587d4e50903a07a19a738549c62
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 13:30:35 +0300

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Define and create an Admin role when Meteor starts.",
-  version: "0.1.0",
+  version: "0.1.1",
   name: "brylie:create-admin-role",
   git: "https://github.com/brylie/meteor-create-admin-role.git"
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.on_use(function (api, where) {
   // Meteor version
   api.versionsFrom("1.0.1");
-  
+
   // Dependencies
   api.use("alanning:roles@1.2.11", "server");
   api.use("brylie:create-role-if-undefined@0.1.0", "server");
