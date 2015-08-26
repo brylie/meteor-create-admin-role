8b9cb6daa3c8aca2ce9905e664e727a193801888
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 12:13:03 +0300

    Use double quotes and spaces

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: "Define and create an Admin role when Meteor starts.",
+  version: "0.1.0",
+  name: "brylie:create-admin-role",
+  git: "https://github.com/brylie/meteor-create-admin-role.git"
+});
+
+Package.on_use(function (api, where) {
+  // Meteor version
+  api.versionsFrom("1.0.1");
+  
+  // Dependencies
+  api.use("alanning:roles@1.2.11", "server");
+  api.use("brylie:create-role-if-undefined@0.1.0", "server");
+
+  // Files
+  api.add_files("server/startup.js", "server");
+});
