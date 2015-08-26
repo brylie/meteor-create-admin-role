8d19386398f3f2ed442d82b54598a8fba3e8524f
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 13:30:28 +0300

    Att try/catch

--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,11 @@
 Meteor.startup(function() {
-	// Define the admin role
-	var adminRole = Meteor.settings.private.adminRoleName || 'admin';
+  // Define the admin role
+  try {
+    var adminRole = Meteor.settings.private.adminRoleName;
+  } catch (error) {
+    var adminRole = 'admin';
+  };
 
-	// Create Admin role if it doesn't exist
-	Meteor.call('createRoleIfNotExisting', adminRoleName);
+  // Create Admin role if it doesn't exist
+  Meteor.call('createRoleIfNotExisting', adminRoleName);
 });
